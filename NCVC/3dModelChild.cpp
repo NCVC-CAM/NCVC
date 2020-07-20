a21c88574d0ea650ec446980f2301a8ac6ca3764
K-Magara <magara@maizuru-ct.ac.jp>
2020-07-20 09:05:01 +0900

    C3dModelChild::OnMDIActivate()

--- NCVC/3dModelChild.cpp
+++ NCVC/3dModelChild.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include "NCVC.h"
+#include "MainFrm.h"
 #include "3dModelChild.h"
 
 #ifdef _DEBUG
@@ -15,6 +16,7 @@
 IMPLEMENT_DYNCREATE(C3dModelChild, CMDIChildWnd)
 
 BEGIN_MESSAGE_MAP(C3dModelChild, CChildBase)
+	ON_WM_MDIACTIVATE()
 END_MESSAGE_MAP()
 
 C3dModelChild::C3dModelChild()
@@ -27,3 +29,14 @@ C3dModelChild::~C3dModelChild()
 
 /////////////////////////////////////////////////////////////////////////////
 // C3dModelChild メッセージ ハンドラー
+
+void C3dModelChild::OnMDIActivate(BOOL bActivate, CWnd* pActivateWnd, CWnd* pDeactivateWnd)
+{
+	__super::OnMDIActivate(bActivate, pActivateWnd, pDeactivateWnd);
+#ifdef _DEBUG
+	printf("C3dModelChild::bActivate=%d\n", bActivate);
+#endif
+	// ﾓｰﾄﾞﾚｽﾀﾞｲｱﾛｸﾞへのﾄﾞｷｭﾒﾝﾄ切替通知
+	if ( bActivate )
+		AfxGetNCVCMainWnd()->AllModelessDlg_PostSwitchMessage();
+}
--- NCVC/3dModelChild.h
+++ NCVC/3dModelChild.h
@@ -13,10 +13,10 @@ class C3dModelChild : public CChildBase
 protected:
 	C3dModelChild();           // 動的生成で使用される protected コンストラクター
 	virtual ~C3dModelChild();
-
-protected:
 	DECLARE_DYNCREATE(C3dModelChild)
+
+public:
+	afx_msg void OnMDIActivate(BOOL bActivate, CWnd* pActivateWnd, CWnd* pDeactivateWnd);
+
 	DECLARE_MESSAGE_MAP()
 };
-
-
