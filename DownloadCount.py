e47a1cfcdfe9456e85e58eccdc1007c6bdd144ad
K-Magara <magara@maizuru-ct.ac.jp>
2023-09-22 16:52:18 +0900

    Update DownloadCount.py

--- DownloadCount.py
+++ DownloadCount.py
@@ -4,7 +4,7 @@ r = requests.get('https://api.github.com/repos/NCVC-CAM/NCVC/releases')
 
 ## リリース昇順にしないと見にくい
 for item in sorted(r.json(), key=lambda k: k['name']):
-    print("ReleaseName: ", item["name"])
+    print("ReleaseName:", item["name"], item["created_at"].split("T")[0])
     for detail in item["assets"]:
         print(" FileName: ", detail["name"], end='')
         print(" download_count=", detail["download_count"])
